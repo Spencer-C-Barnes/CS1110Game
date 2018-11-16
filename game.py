db0e26bb3a66d94788d3f533c349ffaa6246a288
Spencer-C-Barnes <scb9ca@virginia.edu>
2018-11-16 17:41:25 -0500

    Trial

--- game.py
+++ game.py
@@ -5,12 +5,16 @@ import pygame
 
 # Define Camera and Player
 camera = gamebox.Camera(800, 600)
-player = player = gamebox.from_color(50, 50, "blue", 15, 15)
+player = gamebox.from_color(50, 50, "blue", 15, 15)
+
 
 def player_movement(keys):
-    if
+    if pygame.K_a in keys:
+        player.xspeed = 5
+
 
 def tick(keys):
     camera.display()
+
 
 gamebox.timer_loop(30, tick)
